b4ae5c29cbf15465bd9ff5d79f2f6fb2595e1fe1
pavel.koscielny <pawel.koscielny@vodafone.com>
2024-12-05 20:43:23 +0100

    deploy

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,43 +3,46 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import { S3StaticWebsiteOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Nazwa brancha dla unikalnych zasobów
     const branchName = process.env.BRANCH_NAME || "default";
 
-    // Tworzenie bucketu S3
+    // Tworzenie prywatnego bucketu S3
     const bucket = new s3.Bucket(this, `ReactAppBucket-${branchName}`, {
-      websiteIndexDocument: "index.html",
+      websiteIndexDocument: "index.html", // Konfiguracja dla statycznej witryny
       websiteErrorDocument: "index.html",
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Bezpieczne ustawienia
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Prywatny bucket
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatyczne usuwanie przy usuwaniu stacka
     });
 
-    // Tworzenie dystrybucji CloudFront
+    // Konfiguracja CloudFront
     const distribution = new cloudfront.Distribution(
       this,
       `ReactAppDistribution-${branchName}`,
       {
         defaultBehavior: {
-          origin: new S3StaticWebsiteOrigin(bucket), // Użyj poprawnej klasy
-          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+          origin: new origins.S3StaticWebsiteOrigin(bucket), // Użycie poprawnej klasy dla statycznej witryny
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED, // Standardowa polityka cache
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS, // Wymuszenie HTTPS
         },
       }
     );
-    // Wyjście URL dystrybucji
+
+    // Wyjście: URL dystrybucji CloudFront
     new cdk.CfnOutput(this, `CloudFrontURL-${branchName}`, {
       value: distribution.distributionDomainName,
-      description: `CloudFront URL for branch: ${branchName}`,
+      description: "URL to access the deployed React app",
     });
 
+    // Wyjście: Nazwa bucketu S3
     new cdk.CfnOutput(this, `BucketName-${branchName}`, {
       value: bucket.bucketName,
-      description: `S3 Bucket Name for branch: ${branchName}`,
-      exportName: `BucketName-${branchName.replace(/\./g, "-")}`, // Zamień kropki na myślniki
+      description: "Name of the S3 bucket for the React app",
     });
   }
 }
