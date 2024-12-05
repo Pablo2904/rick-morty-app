f96ed0b565b1917c4fdfa02f03e9abf6210f93ab
pavel.koscielny <pawel.koscielny@vodafone.com>
2024-12-05 19:01:06 +0100

    fix deploy

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -9,17 +9,25 @@ export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Pobierz nazwę brancha z zmiennej środowiskowej lub ustaw domyślną wartość
+    const branchName = process.env.BRANCH_NAME || "default";
+    const uniqueId = cdk.Names.uniqueId(this);
+
     // S3 Bucket for hosting the app
-    const bucket = new s3.Bucket(this, "ReactAppBucket", {
-      websiteIndexDocument: "index.html",
-      websiteErrorDocument: "index.html",
-      publicReadAccess: true,
-    });
+    const bucket = new s3.Bucket(
+      this,
+      `ReactAppBucket-${branchName}-${uniqueId}`,
+      {
+        websiteIndexDocument: "index.html",
+        websiteErrorDocument: "index.html",
+        publicReadAccess: true,
+      }
+    );
 
     // CloudFront distribution
     const distribution = new cloudfront.CloudFrontWebDistribution(
       this,
-      "ReactAppDistribution",
+      `ReactAppDistribution-${branchName}-${uniqueId}`,
       {
         originConfigs: [
           {
@@ -33,16 +41,21 @@ export class InfraStack extends Stack {
     );
 
     // Deploy React build files to S3
-    new s3deploy.BucketDeployment(this, "ReactAppDeployment", {
-      sources: [s3deploy.Source.asset("../rick-morty-app-client/build")],
-      destinationBucket: bucket,
-      distribution,
-      distributionPaths: ["/*"], // Invalidate CloudFront cache
-    });
+    new s3deploy.BucketDeployment(
+      this,
+      `ReactAppDeployment-${branchName}-${uniqueId}`,
+      {
+        sources: [s3deploy.Source.asset("../rick-morty-app-client/build")],
+        destinationBucket: bucket,
+        distribution,
+        distributionPaths: ["/*"], // Invalidate CloudFront cache
+      }
+    );
 
     // Output CloudFront URL
-    new cdk.CfnOutput(this, "CloudFrontURL", {
+    new cdk.CfnOutput(this, `CloudFrontURL-${branchName}-${uniqueId}`, {
       value: distribution.distributionDomainName,
+      description: `CloudFront URL for branch: ${branchName}`,
     });
   }
 }
