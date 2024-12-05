a35decdf0ce2038646ba9af7ba6072fb8eb97551
pavel.koscielny <pawel.koscielny@vodafone.com>
2024-12-05 19:16:49 +0100

    deploy fix

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,57 +3,36 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Pobierz nazwę brancha z zmiennej środowiskowej lub ustaw domyślną wartość
+    // Nazwa brancha dla unikalnych zasobów
     const branchName = process.env.BRANCH_NAME || "default";
-    const uniqueId = cdk.Names.uniqueId(this);
 
-    // S3 Bucket for hosting the app
-    const bucket = new s3.Bucket(
-      this,
-      `ReactAppBucket-${branchName}-${uniqueId}`,
-      {
-        websiteIndexDocument: "index.html",
-        websiteErrorDocument: "index.html",
-        publicReadAccess: true,
-        blockPublicAccess: new s3.BlockPublicAccess({
-          blockPublicAcls: false,
-          ignorePublicAcls: false,
-          blockPublicPolicy: false,
-          restrictPublicBuckets: false,
-        }),
-      }
-    );
-
-    // CloudFront distribution
-    const distribution = new cloudfront.CloudFrontWebDistribution(
-      this,
-      `ReactAppDistribution-${branchName}-${uniqueId}`,
-      {
-        originConfigs: [
-          {
-            s3OriginSource: {
-              s3BucketSource: bucket,
-            },
-            behaviors: [{ isDefaultBehavior: true }],
-          },
-        ],
-      }
-    );
-
-    // Deploy React build files to S3
-    new cdk.CfnOutput(this, `CloudFrontURL-${branchName}-${uniqueId}`, {
-      value: distribution.distributionDomainName,
-      description: `CloudFront URL for branch: ${branchName}`,
+    // Tworzenie bucketu S3
+    const bucket = new s3.Bucket(this, `ReactAppBucket-${branchName}`, {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Bezpieczne ustawienia
     });
 
-    // Output CloudFront URL
-    new cdk.CfnOutput(this, `CloudFrontURL-${branchName}-${uniqueId}`, {
+    // Tworzenie dystrybucji CloudFront
+    const distribution = new cloudfront.Distribution(
+      this,
+      `ReactAppDistribution-${branchName}`,
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(bucket), // Bucket jako źródło
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        },
+      }
+    );
+
+    // Wyjście URL dystrybucji
+    new cdk.CfnOutput(this, `CloudFrontURL-${branchName}`, {
       value: distribution.distributionDomainName,
       description: `CloudFront URL for branch: ${branchName}`,
     });
