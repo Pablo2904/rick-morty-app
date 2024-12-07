0b23ccba3ff4b06b98a22a1750369a92107ea8bf
pavel.koscielny <pawel.koscielny@vodafone.com>
2024-12-07 17:09:52 +0100

    fix stack

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -4,26 +4,31 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
-interface InfraStackProps extends StackProps {
-  environment: string; // "production" | "staging"
-}
+
 export class InfraStack extends Stack {
-  constructor(scope: Construct, id: string, props?: InfraStackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const environment = props?.environment || "staging"; // Domyślnie "staging"
-    const bucketName = `rick-morty-bucket-${environment}`; // Nazwa bucketu dla środowiska
+    // Parametr environment
+    const environment = new cdk.CfnParameter(this, "environment", {
+      type: "String",
+      description: "Environment for the stack (e.g., production, staging)",
+      allowedValues: ["production", "staging"], // Lista dopuszczalnych wartości
+      default: "staging",
+    });
+
+    const bucketName = `rick-morty-bucket-${environment.valueAsString}`; // Dynamiczna nazwa bucketu
 
     // Tworzenie prywatnego bucketu S3
     const bucket = new s3.Bucket(this, `ReactAppBucket`, {
       bucketName,
       websiteIndexDocument: "index.html", // Konfiguracja dla statycznej witryny
       websiteErrorDocument: "index.html",
-      // blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Prywatny bucket
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS, // Ustawienie blokady publicznej dla ACL
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatyczne usuwanie przy usuwaniu stacka
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
       publicReadAccess: true,
     });
+
     const originAccessIdentity = new cloudfront.OriginAccessIdentity(
       this,
       "ReactAppOAI",
@@ -32,10 +37,10 @@ export class InfraStack extends Stack {
       }
     );
     bucket.grantRead(originAccessIdentity);
-    // Konfiguracja CloudFront
+
     const distribution = new cloudfront.Distribution(
       this,
-      `ReactAppDistribution-${environment}`,
+      `ReactAppDistribution`,
       {
         defaultBehavior: {
           origin: new origins.S3Origin(bucket, {
@@ -64,17 +69,11 @@ export class InfraStack extends Stack {
 
     new cdk.CfnOutput(this, `CloudFrontID`, {
       value: distribution.distributionId,
-      description: `CloudFront ID for ${environment} environment`,
+      description: `CloudFront ID for ${environment.valueAsString} environment`,
     });
     new cdk.CfnOutput(this, `CloudFrontURL`, {
       value: distribution.distributionDomainName,
-      description: `CloudFront URL for ${environment} environment`,
-    });
-
-    // Wyjście: Nazwa bucketu S3
-    new cdk.CfnOutput(this, `BucketName`, {
-      value: bucket.bucketName,
-      description: `Bucket name for ${environment} environment`,
+      description: `CloudFront URL for ${environment.valueAsString} environment`,
     });
   }
 }
