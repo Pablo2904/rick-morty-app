d5bac5031d35917e403f8b849f15b13c6a47845b
AlicjaSzot <alicja.szot@o2.pl>
2024-12-09 17:57:14 +0100

    Added re-navigate page to main page after 5 seconds for NotFoundPage.

--- rick-morty-app-client/src/pages/404/NotFoundPage.tsx
+++ rick-morty-app-client/src/pages/404/NotFoundPage.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => navigate("/"), 5000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <Container className="text-center mt-5">
