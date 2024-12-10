57d126f437925807e02c2e4521304ced6a935fe3
AlicjaSzot <alicja.szot@o2.pl>
2024-12-10 12:53:21 +0100

    Moved transitions values to _variables file, removed all comments

--- rick-morty-app-client/src/components/atoms/Avatar/Avatar.tsx
+++ rick-morty-app-client/src/components/atoms/Avatar/Avatar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./Avatar.module.scss";
 
-//Def. interfejsu = mówi jakie propsy trzeba przekazac do komponentu
 interface AvatarProps {
   src: string;
   alt: string;
   size?: "small" | "medium" | "large";
 }
 
-//Komponent
 const Avatar: React.FC<AvatarProps> = ({ src, alt, size = "medium" }) => {
   return (
     <img src={src} alt={alt} className={`${styles.avatar} ${styles[size]}`} />
